9b03e8ae7190bef3a849e44713d55bda61f94c74
Humza <humzarao@gmail.com>
2022-10-08 20:33:52 -0400

    Add CSS Styling in HTML and static files

--- base/models.py
+++ base/models.py
@@ -13,4 +13,4 @@ class Task(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['complete']
+        ordering = ['complete','-create']
--- base/views.py
+++ base/views.py
@@ -46,7 +46,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['complete'] = context['tasks'].filter(complete=False).count()
+        context['count'] = context['tasks'].filter(complete=False).count()
         
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
